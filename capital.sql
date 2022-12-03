8bfbb92d6db49032e03d7a2c60cfcb079e292124
Maycol07 <1353921qsenati.pe>
2022-12-03 17:57:40 -0500

    sql

--- capital.sql
+++ capital.sql
@@ -2,15 +2,15 @@ use world;
 
 DELIMITER $
 
-CREATE PROCEDURE mostrar4(in pais varchar(155))
+CREATE PROCEDURE mostrar6(in pais varchar(155))
 BEGIN
 
 SELECT country.name, country.capital, city.name, countrylanguage.Language FROM city
  INNER JOIN country ON city.countrycode=country.code
  INNER JOIN         
  countrylanguage ON Countrylanguage.countrycode=country.code
- WHERE country.name=pais;
+ WHERE country.name=pais group by country.name;
  
 END$
 
-CALL mostrar4('china')
+CALL mostrar6('chile')
